42319ebc78862b1328db2209250ce68fb550c914
justinthelaw <justinthelaw@gmail.com>
2023-12-14 22:44:23 -0500

    fix fetch-model script tokenizer

--- scripts/download_guidance_model.py
+++ scripts/download_guidance_model.py
@@ -1,6 +1,8 @@
-from transformers import LongT5ForConditionalGeneration
+from transformers import LongT5ForConditionalGeneration, T5TokenizerFast
 
 # TODO: try out pszemraj/long-t5-tglobal-base-16384-book-summary instead
 model = LongT5ForConditionalGeneration.from_pretrained("google/long-t5-tglobal-large")
+tokenizer = T5TokenizerFast.from_pretrained("google/long-t5-tglobal-large")
 
 model.save_pretrained(".model")
+tokenizer.save_pretrained(".model")
